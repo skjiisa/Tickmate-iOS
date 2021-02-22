56d66c48a50f1ae586354e58649604d76055eb55
Isvvc <lyons@tuta.io>
2021-02-22 15:27:37 -0700

    Create function to load a track's ticks

--- Tickmate/Tickmate/Controllers/TickController.swift
+++ Tickmate/Tickmate/Controllers/TickController.swift
@@ -5,14 +5,52 @@
 //  Created by Isaac Lyons on 2/21/21.
 //
 
-import Foundation
+import CoreData
+import SwiftDate
 
 class TickController: ObservableObject {
     
     let track: Track
+    @Published var ticks: [Set<Tick>] = []
     
     init(track: Track) {
         self.track = track
+        loadTicks()
+    }
+    
+    func loadTicks() {
+        let fetchRequest: NSFetchRequest<Tick> = Tick.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Tick.timestamp, ascending: false)]
+        guard let ticks = try? track.managedObjectContext?.fetch(fetchRequest),
+              let today = Date().dateTruncated([.hour, .minute, .second]) else { return }
+        
+        var tickDays: [(day: Int, tick: Tick)] = []
+        
+        for tick in ticks {
+            guard let timestamp = tick.timestamp?.dateTruncated([.hour, .minute, .second]),
+                  let days = (today - timestamp).day else { continue }
+            guard days < 365 else { break }
+            tickDays.append((days, tick))
+        }
+        
+        var days: [Set<Tick>] = []
+        var i = 0
+        
+        for day in 0..<365 {
+            var dayTicks = Set<Tick>()
+            while i < tickDays.count,
+                  tickDays[i].day == day {
+                dayTicks.insert(tickDays[i].tick)
+                i += 1
+            }
+            
+            days.append(dayTicks)
+            
+            guard i < tickDays.count else { break }
+        }
+        
+        self.ticks = days
+        print(self.ticks)
     }
     
 }
--- Tickmate/Tickmate/Controllers/TrackController.swift
+++ Tickmate/Tickmate/Controllers/TrackController.swift
@@ -11,4 +11,12 @@ class TrackController: ObservableObject {
     
     private var tickControllers: [Track: TickController] = [:]
     
+    func loadTicks(for track: Track) {
+        if let tickController = tickControllers[track] {
+            tickController.loadTicks()
+        } else {
+            tickControllers[track] = TickController(track: track)
+        }
+    }
+    
 }
