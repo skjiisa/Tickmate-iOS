fbaea1dd0a014328eac8f9986b4192b33cf8eea1
Isvvc <lyons@tuta.io>
2021-02-23 19:30:57 -0700

    Create color rgb conversions

---
+++ Tickmate/Tickmate/Supplementary Views/Color+RGB.swift
@@ -0,0 +1,32 @@
+//
+//  Color+RGB.swift
+//  Tickmate
+//
+//  Created by Isaac Lyons on 2/23/21.
+//
+
+import SwiftUI
+
+extension Color {
+    /// Returns the color (excluding alpha) as an RGB color.
+    var rgb: Int? {
+        guard let components = cgColor?.components,
+              components.count >= 3 else {
+            return nil
+        }
+        
+        let r = Int(components[0] * 255)
+        let g = Int(components[1] * 255)
+        let b = Int(components[2] * 255)
+        return r << 16 + g << 8 + b
+    }
+    
+    /// Initialize based on a 24-bit integer RGB value, such as from a hex code. Does not support alpha channel.
+    /// - Parameter rgb: An integer RGB value without alpha.
+    init(rgb: Int) {
+        let r = Double((rgb & 0xff0000) >> 16) / 255
+        let g = Double((rgb & 0x00ff00) >> 8) / 255
+        let b = Double((rgb & 0x0000ff)) / 255
+        self.init(red: r, green: g, blue: b)
+    }
+}
