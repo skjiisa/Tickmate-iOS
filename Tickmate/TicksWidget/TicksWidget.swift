9445f24ff7d5586251df1ef5a65793cef9a5e817
Isvvc <lyons@tuta.io>
2021-06-24 20:08:34 -0600

    Rename SimpleEntry to TracksEntry
    Remove test property from TracksEntry
    Improve TicksWidget preview

--- Tickmate/TicksWidget/TicksWidget.swift
+++ Tickmate/TicksWidget/TicksWidget.swift
@@ -11,17 +11,17 @@ import Intents
 import CoreData
 
 struct Provider: IntentTimelineProvider {
-    func placeholder(in context: Context) -> SimpleEntry {
-        SimpleEntry(date: Date(), configuration: ConfigurationIntent(), test: "Placeholder")
+    func placeholder(in context: Context) -> TracksEntry {
+        TracksEntry(date: Date(), configuration: ConfigurationIntent())
     }
 
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (SimpleEntry) -> ()) {
-        let entry = SimpleEntry(date: Date(), configuration: configuration, test: "Snapshot")
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (TracksEntry) -> ()) {
+        let entry = TracksEntry(date: Date(), configuration: configuration)
         completion(entry)
     }
 
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> ()) {
-        var entries: [SimpleEntry] = []
+        var entries: [TracksEntry] = []
 
         // Generate a timeline consisting of five entries an hour apart, starting from the current date.
         /*
@@ -39,25 +39,22 @@ struct Provider: IntentTimelineProvider {
         fetchRequest.fetchLimit = 4
         
         let tracks = try? context.fetch(fetchRequest)
-        let name = tracks?.compactMap { $0.name }.joined(separator: ", ") ??? "Timeline"
         
-        entries.append(Entry(date: Date(), configuration: configuration, test: name, tracks: tracks ?? []))
+        entries.append(Entry(date: Date(), configuration: configuration, tracks: tracks ?? []))
 
         let timeline = Timeline(entries: entries, policy: .atEnd)
         completion(timeline)
     }
 }
 
-struct SimpleEntry: TimelineEntry {
+struct TracksEntry: TimelineEntry {
     let date: Date
     let configuration: ConfigurationIntent
-    let test: String
     let tracks: [Track]
     
-    internal init(date: Date, configuration: ConfigurationIntent, test: String, tracks: [Track] = []) {
+    internal init(date: Date, configuration: ConfigurationIntent, tracks: [Track] = []) {
         self.date = date
         self.configuration = configuration
-        self.test = test
         self.tracks = tracks
     }
 }
@@ -94,8 +91,11 @@ struct TicksWidget: Widget {
 }
 
 struct TicksWidget_Previews: PreviewProvider {
+    static let trackController = TrackController(observeChanges: false, preview: true)
+    
     static var previews: some View {
-        TicksWidgetEntryView(entry: SimpleEntry(date: Date(), configuration: ConfigurationIntent(), test: "Preview"))
+        TicksWidgetEntryView(entry: TracksEntry(date: Date(), configuration: ConfigurationIntent(), tracks: Array(trackController.fetchedResultsController.fetchedObjects!.prefix(4))))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
+            .environmentObject(trackController)
     }
 }
