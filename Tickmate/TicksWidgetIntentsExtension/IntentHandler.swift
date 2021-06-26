8cd75c487f65ae24ab0dddcb3a8c842433226c33
Isvvc <lyons@tuta.io>
2021-06-25 22:49:22 -0600

    Add configurable Tracks list

---
+++ Tickmate/TicksWidgetIntentsExtension/IntentHandler.swift
@@ -0,0 +1,36 @@
+//
+//  IntentHandler.swift
+//  TicksWidgetIntentsExtension
+//
+//  Created by Isaac Lyons on 6/24/21.
+//
+
+import Intents
+import CoreData
+
+class IntentHandler: INExtension, ConfigurationIntentHandling {
+    
+    override func handler(for intent: INIntent) -> Any {
+        // This is the default implementation.  If you want different objects to handle different intents,
+        // you can override this and return the handler you want for that particular intent.
+        
+        return self
+    }
+    
+    func provideTracksOptionsCollection(for intent: ConfigurationIntent, with completion: @escaping (INObjectCollection<TrackItem>?, Error?) -> Void) {
+        let fetchRequest: NSFetchRequest<Track> = Track.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor(keyPath: \Track.index, ascending: true)]
+        
+        do {
+            let context = PersistenceController.shared.container.viewContext
+            let tracks: [TrackItem] = try context.fetch(fetchRequest).compactMap { track in
+                let id = track.objectID.uriRepresentation().absoluteString
+                return TrackItem(identifier: id, display: track.name ??? "New Track")
+            }
+            completion(INObjectCollection(items: tracks), nil)
+        } catch {
+            completion(nil, error)
+        }
+    }
+    
+}
