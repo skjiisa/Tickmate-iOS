119fec86afc8f9484f02de2da000ec1e54ae349a
Elaine Lyons <elaine@lyons.app>
2025-04-06 14:08:47 -0600

    v1.5 tweaks (#100)

--- Tickmate/Tickmate/TickmateApp.swift
+++ Tickmate/Tickmate/TickmateApp.swift
@@ -10,6 +10,7 @@ import SwiftUI
 @main
 struct TickmateApp: App {
     let persistenceController = PersistenceController.shared
+        //.loadDemo() // Uncomment this to load demo data into a fresh install
 
     var body: some Scene {
         WindowGroup {
